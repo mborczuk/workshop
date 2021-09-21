3fdf8bb848736c9cea221eefc56d2aa6ae740a19
Michael Borczuk <46628023+mborczuk@users.noreply.github.com>
2021-09-21 09:26:01 -0400

    add absent work from 2021-09-21

---
+++ PrintNames.py
@@ -0,0 +1,5 @@
+import random
+pd1 = ["Owen Yaggy", "Haotian Gan", "Ishraq Mahid", "Michelle Lo","john0" ,"john1", "john2", "john3", "john4", "john5","john6" ,"john7", "john8", "john9", "john10", "john11"]
+pd2 = ["Patrick Ging", "Raymond Yeung", "Kevin Cao", "Ivan Lam", "jane0" ,"jane1", "jane2", "jane3", "jane4", "jane5","jane6" ,"jane7", "jane8", "jane9", "jane10", "jane11"]
+print("pd1 name: " + str(pd1[int(random.random() * len(pd1))]))
+print("pd2 name: " + str(pd2[int(random.random() * len(pd2))]))
